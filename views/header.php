88a10d200ac49002a7de472ebbea3fb50c5ac379
João Pedro <45838334+ribe3iro@users.noreply.github.com>
2021-09-21 16:36:34 -0300

    Removing some unnecessary comments

--- views/header.php
+++ views/header.php
@@ -4,9 +4,7 @@
 <html lang="pt-br">
 <head>
     <title>MVC Training</title>
-    <!-- <link rel="stylesheet" href="<?=URL?>public/css/default.css"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="https://raw.githubusercontent.com/StartBootstrap/startbootstrap-sb-admin-2/master/css/sb-admin-2.min.css" type="text/css"> -->
     <script type='text/javascript' src='https://code.jquery.com/jquery-3.6.0.min.js'></script>
     <!-- <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script> -->
     <!-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script> -->
